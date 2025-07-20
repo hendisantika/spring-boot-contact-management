b770fa2c6a565be7ed86cf24ab963a623d4b4678
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 15:12:38 +0700

    feat: Add CustomUserDetails🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/contactmanagement/config/CustomUserDetails.java
@@ -0,0 +1,74 @@
+package id.my.hendisantika.contactmanagement.config;
+
+import id.my.hendisantika.contactmanagement.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-contact-management
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/07/25
+ * Time: 15.11
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomUserDetails implements UserDetails {
+
+
+    private final User user;
+
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole());
+
+        return List.of(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
+    }
+}
