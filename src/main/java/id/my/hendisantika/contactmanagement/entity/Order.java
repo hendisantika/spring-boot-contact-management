b18dd4a8f7e1a2fe9eae69a34d713c1c0aca3a15
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 15:04:56 +0700

    feat: Add Order Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/contactmanagement/entity/Order.java
@@ -0,0 +1,43 @@
+package id.my.hendisantika.contactmanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-contact-management
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/07/25
+ * Time: 15.04
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long myOrderId;
+    private String orderId;
+    private double amount;
+    private String receipt;
+    private String status;
+
+    @ManyToOne
+    private User user;
+
+    private String paymentId;
+}
