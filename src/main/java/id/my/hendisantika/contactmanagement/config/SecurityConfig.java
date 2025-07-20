8e654e081d34986f8f59464ac4b3afcf6c47fa15
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 15:13:13 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/contactmanagement/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.contactmanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-contact-management
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/07/25
+ * Time: 15.12
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
