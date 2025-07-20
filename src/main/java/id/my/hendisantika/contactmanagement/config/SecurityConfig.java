9bbc970a58d2babedffff25d49705c43cbcfc698
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 15:14:18 +0700

    feat: Add configure in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/contactmanagement/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/contactmanagement/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.contactmanagement.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,6 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
         return daoAuthenticationProvider;
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
     }
 }
