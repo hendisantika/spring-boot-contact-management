b41778bfa955799fe996a0791e1749a3b79b8b7b
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 14:56:49 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/contactmanagement/SpringBootContactManagementApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.contactmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootContactManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootContactManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/contactmanagement/SpringBootContactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.contactmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContactManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
