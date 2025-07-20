925f6fe031a24cb3691a4692b7f592942676ea90
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 15:27:41 +0700

    feat: Add search in SearchController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/contactmanagement/controller/SearchController.java
+++ src/main/java/id/my/hendisantika/contactmanagement/controller/SearchController.java
@@ -1,9 +1,17 @@
 package id.my.hendisantika.contactmanagement.controller;
 
+import id.my.hendisantika.contactmanagement.entity.Contact;
+import id.my.hendisantika.contactmanagement.entity.User;
 import id.my.hendisantika.contactmanagement.repository.ContactRepository;
 import id.my.hendisantika.contactmanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +32,10 @@ public class SearchController {
 
     private final ContactRepository contactRepository;
 
+    @GetMapping("/search/{keyword}")
+    public ResponseEntity<?> search(@PathVariable("keyword") String keyword, Principal p) {
+        User user = userRepository.getUserByUserName(p.getName());
+        List<Contact> contacts = contactRepository.findByUidAndKeyword(user.getUid(), keyword);
+        return ResponseEntity.ok(contacts);
+    }
 }
