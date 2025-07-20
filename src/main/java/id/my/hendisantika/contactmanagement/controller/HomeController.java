d0b7b5c740da70e92d936ef35be31b43f22dec55
hendisantika <hendisantika@yahoo.co.id>
2025-07-20 15:26:04 +0700

    feat: Add signupUser in HomeController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/contactmanagement/controller/HomeController.java
+++ src/main/java/id/my/hendisantika/contactmanagement/controller/HomeController.java
@@ -1,12 +1,20 @@
 package id.my.hendisantika.contactmanagement.controller;
 
+import id.my.hendisantika.contactmanagement.dto.MessageDTO;
 import id.my.hendisantika.contactmanagement.entity.User;
 import id.my.hendisantika.contactmanagement.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,6 +27,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Time: 15.21
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
@@ -50,5 +59,53 @@ public class HomeController {
         model.addAttribute("title", "Signup - Smart Contact Manager");
         model.addAttribute("user", new User());
         return "signup";
+    }
+
+    @PostMapping(value = "/do_signup")
+    public String signupUser(@Valid @ModelAttribute("user") User user, BindingResult r, Model model, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, HttpSession session) {
+        try {
+            if (!agreement) {
+                log.info("You have not agreed the Terms and Conditions");
+                throw new Exception("You have not agreed the Terms and Conditions");
+            }
+
+            if (r.hasErrors()) {
+                model.addAttribute("user", user);
+                log.info("Error {}", r);
+                return "signup";
+            }
+
+
+            if (userRepository.getUserByUserName(user.getEmail()) != null) {
+                log.info("Same Email Id is already registered. Try using different Email Id !");
+                throw new Exception("Same Email Id is already registered. Try using different Email Id !");
+            }
+
+            user.setRole("ROLE_USER");
+            user.setEnabled(true);
+            user.setImage("default.png");
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+
+            log.info("Agreement {}", agreement);
+            log.info("Userr {}", user);
+
+            User result = this.userRepository.save(user);
+
+            // it means it will return the data on the same page
+            model.addAttribute("user", new User());
+
+            session.setAttribute("message", new MessageDTO("Successfully registered !!", "alert-success"));
+            return "signup";
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("user", user);
+            session.setAttribute("message", new MessageDTO("Something went wrong !!" + e.getMessage(), "alert-danger"));
+            return "signup";
+        }
+
+
     }
 }
